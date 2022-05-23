6db127d904be60962fcc633ed299921888e7bed0
James <2jcjl4@gmail.com>
2022-05-23 16:50:49 +0100

    end of lesson, connecting to h2

--- SpringbootExample/src/main/java/com/qa/SpringbootExample/SpringbootExampleApplication.java
+++ SpringbootExample/src/main/java/com/qa/SpringbootExample/SpringbootExampleApplication.java
@@ -9,21 +9,8 @@ import org.springframework.context.annotation.Bean;
 public class SpringbootExampleApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(SpringbootExampleApplication.class, args);
+		SpringApplication.run(SpringbootExampleApplication.class, args);
 		
-	   
-
-	    Object byName = context.getBean("Hello");
-	    String byType = context.getBean(String.class);
-	    String byBoth = context.getBean("Hello", String.class);
-
-	    System.out.println(byName);
-	    System.out.println(byType);
-	    System.out.println(byBoth);
 	}
 
-	   @Bean
-	    public String Hello() {
-	        return "Hello, World";
-	    }
 }
